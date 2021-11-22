c0eace592e07cd95bdfc45f10c2d30e8f76bf9aa
Yassine Elhilali <elhilaliyacin@gmail.com>
2021-11-22 21:18:22 +0100

    first commit

---
+++ src/main/java/hackernews/Query.java
@@ -0,0 +1,18 @@
+package hackernews;
+
+import java.util.List;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+
+public class Query implements GraphQLQueryResolver {
+
+	private final LinkRepository linkRepository;
+
+	public Query(LinkRepository linkRepository) {
+		this.linkRepository = linkRepository;
+	}
+
+	public List<Link> allLinks() {
+		return linkRepository.getAllLinks();
+	}
+
+}
